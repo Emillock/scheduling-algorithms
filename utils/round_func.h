903a2043983f53f59371f07f7d3e40177a048e73
Emillock <mirzazadeemil2005@gmail.com>
2025-03-28 23:08:44 +0400

    changed the code of round2D function for better rounding

--- utils/round_func.h
+++ utils/round_func.h
@@ -4,11 +4,11 @@
 
 #ifndef UNTITLED3_ROUND_FUNC_H
 #define UNTITLED3_ROUND_FUNC_H
+#include <cmath>  // For round()
 
 float round2D(float var)
 {
-    float value = (int)(var * 100 + .5);
-    return (float)value / 100;
+    return std::round(var * 100.0) / 100.0;
 }
 
 #endif //UNTITLED3_ROUND_FUNC_H
